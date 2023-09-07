1652bbc6e1bef6ee4c1081a247ba1c9348995185
ahmedemam777 <ahmedemam201028@gmail.com>
2023-09-07 08:11:00 -0700

    6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int a;
+char b;
+float c;
+printf("size is %d",sizeof(a));
+printf("size is %s",sizeof(b));
+printf("size is %f",sizeof(c));
+}
