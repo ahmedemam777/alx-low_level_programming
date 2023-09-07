4f8cc256f627dbfbd6fe5371a2a8871ce2df120a
ahmedemam777 <ahmedemam201028@gmail.com>
2023-09-07 08:16:41 -0700

    6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -9,7 +9,12 @@ int main(void)
 int a;
 char b;
 float c;
-printf("size is %d",sizeof(a));
-printf("size is %s",sizeof(b));
-printf("size is %f",sizeof(c));
+long int d;
+long long int f;
+printf("size is %d", sizeof(a));
+printf("size is %s", sizeof(b));
+printf("size is %f", sizeof(c));
+printf("size is %lu", sizeof(d));
+printf("size is %lu", sizeof(f));
+return (0);
 }
