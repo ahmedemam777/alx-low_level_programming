bf143da81663305834a7168b1b8aa6b8f3116fb9
ahmedemam777 <ahmedemam201028@gmail.com>
2023-09-08 15:23:00 -0700

    3

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -6,14 +6,14 @@
 */
 int main(void)
 {
-          char x;
-          for(x = 'a'; x <= 'z'; x++)
-	    {putchar(x);}
+char x;
+for(x = 'a'; x <= 'z'; x++)
+{putchar(x);}
 
-          for(x = 'A'; x <= 'Z'; x++)
-	    {putchar(x);}
+for(x = 'A'; x <= 'Z'; x++)
+{putchar(x);}
 
-          putchar('\n');
+putchar('\n');
 
-          return (0);        
+return (0);        
 }
