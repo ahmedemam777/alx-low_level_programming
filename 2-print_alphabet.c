aff7ad42fab04c559480396a32311fe779294dc9
ahmedemam777 <ahmedemam201028@gmail.com>
2023-09-08 14:45:24 -0700

    2

---
+++ 2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ *main -> program that prints the alphabet in lowercase, followed by a new line
+ *
+ *Return: always 0.
+ */
+
+int main(void)
+{
+char x;
+for(x = "a"; x<= "z"; x++)
+{
+putchar(x);
+}
+putchar("\n");
+return (0);
+}
