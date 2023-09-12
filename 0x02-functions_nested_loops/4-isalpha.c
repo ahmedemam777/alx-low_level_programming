4e0b01a2f9fe02f2f1f80222a78a8518d788713d
ahmedemam777 <ahmedemam201028@gmail.com>
2023-09-12 13:52:49 -0700

    4

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _isalpha - check if input is alohabetical character or not
+ * Return: 1 if alphabet and 0 otherwise
+ */
+
+int _isalpha(int c){
+
+if ((c >= 'a' && c <= 'z' ) || (c >= 'A' && C <= 'Z'))
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
