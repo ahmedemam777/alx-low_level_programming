ac23b199b2f4c1aa37c010d1c3828e707cd9ecc0
ahmedemam777 <ahmedemam201028@gmail.com>
2023-09-18 08:52:23 -0700

    0 task

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * void reset_to_98 - take pointer as int and change the value
+ * @n: in the check
+ * Return: 0 in succision
+ */
+void reset_to_98(int *n)
+{
+*n = 98;
+}
