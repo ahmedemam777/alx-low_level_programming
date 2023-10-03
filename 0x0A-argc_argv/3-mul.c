d2329a90368f6238f2c0fad747aeea6a899f2ad9
ahmedemam777 <ahmedemam201028@gmail.com>
2023-10-03 08:55:02 -0700

    3

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - print the multiplication of two number
+ * @argc: arg number
+ * @argv: arguments
+ * Return: 0
+ */
+int main(int argc, char **argv)
+{
+int x, y;
+if (argc < 3)
+{
+printf("Error\n")
+return (1);
+}
+x = atoi(argv[1]);
+y = atoi(argv[2]);
+printf("%d\n", x * y);
+}
