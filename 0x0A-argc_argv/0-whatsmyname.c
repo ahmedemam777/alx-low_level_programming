9fb5203d721a00ebfc816a4f33949fd281286b15
ahmedemam777 <ahmedemam201028@gmail.com>
2023-10-03 08:14:58 -0700

    0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - print its name followed by new line
+ * @argc: argument count
+ * @argv: arguments
+ * Return: 0 always
+ */
+int main(int argc, char **argv)
+{
+(void)argc;
+printf("%s\n", argv[0]);
+return (0);
+}
